c3801ff2204b5418925c73d520b093d0d18c668f
xaosland <xaosland@mail.ru>
2023-09-12 18:45:37 +0300

    task 7

--- lesson-01/04.js
+++ lesson-01/04.js
@@ -1,9 +1,6 @@
 let x = 5;
-x = x += 3;
-console.log(x)
-x = x *= 2;
-console.log(x)
-x = x -= 4;
-console.log(x)
-x = x /= 3;
+x += 3;
+x *= 2;
+x -= 4;
+x /= 3;
 console.log(x)
---
+++ lesson-01/07.js
@@ -0,0 +1,5 @@
+let iAm = "я";
+let krasavchik = "красавчик!";
+let upper = iAm.toUpperCase();
+
+console.log(upper + " " + krasavchik)
