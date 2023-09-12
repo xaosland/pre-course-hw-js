c9f47b2a4486155c6131e88452b5da95296341e6
xaosland <xaosland@mail.ru>
2023-09-12 18:45:26 +0300

    task 6

--- lesson-01/04.js
+++ lesson-01/04.js
@@ -1,9 +1,9 @@
 let x = 5;
-x += 3;
+x = x += 3;
 console.log(x)
-x *= 2;
+x = x *= 2;
 console.log(x)
-x -= 4;
+x = x -= 4;
 console.log(x)
-x /= 3;
+x = x /= 3;
 console.log(x)
---
+++ lesson-01/06.js
@@ -0,0 +1,6 @@
+let str = "zzzzyzz"
+if (str.includes = "y") {
+    console.log("нашел")
+} else {
+    console.log("заблудился")
+}
