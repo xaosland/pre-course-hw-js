b706dc955535fad6ee8cd2e1e61799fef7a8cece
xaosland <xaosland@mail.ru>
2023-09-18 18:55:21 +0300

    tsk4

--- lesson-02/04.js
+++ lesson-02/04.js
@@ -1,5 +1,5 @@
 const giveMeRightCondition = (value1, value2, maximum) => {
-    if (value1 <= value2 === maximum) {
+    if (value1 <= value2 && maximum) {
         console.log('Success!');
     } else {
         console.log('Something wrong!');
