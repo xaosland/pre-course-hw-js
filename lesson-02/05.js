1f6f7477d96ff18e9ec6e3b071f09153deb4553b
xaosland <xaosland@mail.ru>
2023-09-19 14:17:30 +0300

    tsk5

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -1,5 +1,4 @@
 let passport = { name: "Petr", surname: "Petrov" };
-console.log(passport);
-
 let passportCopy = {...passport, name: "Ivan"};
-console.log(passportCopy);
+console.log(passportCopy)
+console.log(passport)
