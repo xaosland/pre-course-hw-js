f44cd6e399988d6cd80f5872b9f17bea73a03134
xaosland <xaosland@mail.ru>
2023-09-19 15:44:01 +0300

    tsk6

--- lesson-02/06.js
+++ lesson-02/06.js
@@ -1,13 +1,16 @@
 let passportWithAddress = {
-    name: "Petr",
-    surname: "Petrov",
-    address: {
-        country: "USA",
-        city: "LA"
-    }
+  name: "Petr",
+  surname: "Petrov",
+  address: {
+    country: "USA",
+    city: "LA",
+  },
 };
 
-let passportWithAddressCopy = {...passportWithAddress, address:city = "Bobryisk"}
+let passportWithAddressCopy = {
+  ...passportWithAddress,
+  address: (city = "Bobryisk"),
+};
 
-console.log(passportWithAddress)
-console.log(passportWithAddressCopy)
+console.log(passportWithAddress);
+console.log(passportWithAddressCopy);
