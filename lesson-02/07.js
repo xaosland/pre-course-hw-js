bea6b579be6adda406a7afa6d1b102b0382f6188
xaosland <xaosland@mail.ru>
2023-09-20 19:00:03 +0400

    tsk7

---
+++ lesson-02/07.js
@@ -0,0 +1,14 @@
+let passportMarried = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+
+let passportMarriedCopy = Object.assign ({}, passportMarried)
+passportMarriedCopy["married"] = true
+
+console.log(passportMarried);
+console.log(passportMarriedCopy);
