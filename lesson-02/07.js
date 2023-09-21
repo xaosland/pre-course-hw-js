ef338d60dfec7668a2daeda14903ea38ba88c8ea
xaosland <xaosland@mail.ru>
2023-09-21 09:27:27 +0300

    tsk7

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -7,8 +7,7 @@ let passportMarried = {
     }
 };
 
-let passportMarriedCopy = Object.assign ({}, passportMarried)
-passportMarriedCopy["married"] = true;
-
-console.log(passportMarried);
-console.log(passportMarriedCopy);
+let passportMarried2 = { ...passportMarried, married: true}
+ 
+console.log(passportMarried); 
+console.log(passportMarried2);
