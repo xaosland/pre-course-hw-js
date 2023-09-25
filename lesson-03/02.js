5de25d79fb3d6d57f92595e65bb43d72858cf2f5
xaosland <xaosland@mail.ru>
2023-09-25 13:38:44 +0400

    ts3

---
+++ lesson-03/02.js
@@ -0,0 +1,8 @@
+const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
+const index = words.indexOf("апельсин");
+
+if (index !== -1) {
+  console.log("Ура! нашел");
+} else {
+  console.log("Придется поискать в другом магазине…");
+}
