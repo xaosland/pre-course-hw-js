10e957538915c5f66996750ec4238df657430edd
xaosland <xaosland@mail.ru>
2023-09-25 15:25:51 +0400

    ts3

---
+++ lesson-03/03.js
@@ -0,0 +1,4 @@
+const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
+
+const fruitSlice = fruits.slice(0,1) + "," + fruits.slice(-2);
+console.log(fruitSlice)
