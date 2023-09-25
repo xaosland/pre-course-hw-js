7eaff3c7f5254d38ebf1bcb3b9d543914b1e1ab8
xaosland <xaosland@mail.ru>
2023-09-25 12:21:05 +0400

    tsk3

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,7 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return colors.filter(el => el === "черный" || el === "красный" || el === "желтый")
+}
+
+console.log(createColorString());
