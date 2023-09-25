bd622232b7a7cadfff7b617d9264ed6924131454
xaosland <xaosland@mail.ru>
2023-09-25 16:35:03 +0400

    ts4

--- lesson-03/04.js
+++ lesson-03/04.js
@@ -1,22 +1,24 @@
-const users = [ 
-    { 
-        id: 1, 
-        name: 'John', 
-        surName: 'Doe', 
-        address: '123 Main St' 
-    }, 
-    { 
-        id: 2, 
-        name: 'Jane', 
-        surName: 'Smith', 
-        address: '456 Elm St' 
-    }, 
-    { 
-        id: 3, 
-        name: 'Tom', 
-        surName: 'Johnson', 
-        address: '789 Oak St' 
-    } 
-]; 
+const users = [
+  {
+    id: 1,
+    name: "John",
+    surName: "Doe",
+    address: "123 Main St",
+  },
+  {
+    id: 2,
+    name: "Jane",
+    surName: "Smith",
+    address: "456 Elm St",
+  },
+  {
+    id: 3,
+    name: "Tom",
+    surName: "Johnson",
+    address: "789 Oak St",
+  },
+];
 
-console.log(users.filter(`id`));
+users.forEach((user) => {
+  console.log(user.address);
+});
