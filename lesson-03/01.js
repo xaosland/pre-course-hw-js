b11c9587ca7313b715d39f5309ff49e2312eb6e1
xaosland <xaosland@mail.ru>
2023-09-25 12:57:20 +0400

    ts3

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,7 +1,7 @@
 const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 
 function createColorString() {
-    return colors.filter(el => el === "черный" || el === "красный" || el === "желтый")
+    return colors.filter(el => el === "черный" || el === "красный" || el === "желтый");
 }
 
-console.log(createColorString());
+console.log(createColorString().join('-')) 
