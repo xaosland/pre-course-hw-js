a6cf8c7916529dc95c6d060be701d4e70ea5dc4e
xaosland <xaosland@mail.ru>
2023-09-25 22:35:09 +0400

    ts7

--- lesson-03/07.js
+++ lesson-03/07.js
@@ -2,7 +2,10 @@ const numbers = [
     [1, 2, 3, 4, 5],
     [11, 20, 33, 40, 55],
     [111, 200, 333, 400, 555],
-];
-
-const zzz = numbers.filter((el) => el === 0);
-console.log(zzz);
+  ];
+  
+  const numbers2 = numbers.map((innerArray) => {
+    return innerArray.filter((number) => number % 2 === 0);
+  });
+  
+  console.log(numbers2);
